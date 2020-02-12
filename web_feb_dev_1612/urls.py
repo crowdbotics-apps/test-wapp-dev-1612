4237b3f303203cf009199c181330cd3f901d45f5
Crowdbotics <team@crowdbotics.com>
2020-02-12 12:59:54 +0000

    Update admin title

--- web_feb_dev_1612/urls.py
+++ web_feb_dev_1612/urls.py
@@ -33,9 +33,9 @@ urlpatterns = [
     path("home/", include("home.urls")),
 ]
 
-admin.site.site_header = "web_feb"
-admin.site.site_title = "web_feb Admin Portal"
-admin.site.index_title = "web_feb Admin"
+admin.site.site_header = "Test_App"
+admin.site.site_title = "Test_App Admin Portal"
+admin.site.index_title = "Test_App Admin"
 
 # swagger
 schema_view = get_schema_view(
